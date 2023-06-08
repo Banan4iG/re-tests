21968cbb2385f416e7803d32b2f36f926462f695
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-06-08 17:28:44 +0300

    Add test to check correct opening query editor. See RS-98654

---
+++ tests/test_check_open_query_editor.py
@@ -0,0 +1,59 @@
+import lackey
+import os
+from re_tests_plugin import *
+
+
+def test_empty_saved():
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result1 = lackey.exists("tab_query_editor_text.png")
+    lackey.doubleClick("icon_conn_open.png")
+
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result2 = lackey.exists("tab_query_editor_text.png")
+    lackey.doubleClick("icon_conn_open.png")
+
+    assert result1 != None
+    assert result2 != None
+
+def test_empty():
+    home_dir = os.path.expanduser("~")
+    os.remove(os.path.join(home_dir,'.redexpert/202301/QueryEditor/script1.sql'))
+    
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result1 = lackey.exists("tab_query_editor_text.png")
+    lackey.doubleClick("icon_conn_open.png")
+
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result2 = lackey.exists("tab_query_editor_text.png")
+    lackey.doubleClick("icon_conn_open.png")
+
+    lackey.type("s", lackey.Key.CTRL)
+
+    assert result1 != None
+    assert result2 != None
+
+def test_saved():
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    lackey.App.setClipboard("select * from employee;")
+    lackey.type("v", lackey.Key.CTRL)
+    lackey.doubleClick("icon_conn_open.png")
+
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result1 = lackey.exists("text_query_1.png")
+    lackey.doubleClick("icon_conn_open.png")
+
+    lackey.doubleClick("icon_conn.png")
+    time.sleep(2)
+    result2 = lackey.exists("text_query_1.png")
+    lackey.type("a", lackey.Key.CTRL)
+    lackey.type(lackey.Key.BACKSPACE)
+    lackey.doubleClick("icon_conn_open.png")
+
+    assert result1 != None
+    assert result2 != None
