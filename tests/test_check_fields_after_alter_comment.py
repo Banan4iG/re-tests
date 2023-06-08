44b2b77455339d3f5c68dadd1efcf27a7af10e4a
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-06-08 17:32:01 +0300

    Add test to check if fields of the table do not disappear. See RS-106690

---
+++ tests/test_check_fields_after_alter_comment.py
@@ -0,0 +1,30 @@
+import lackey
+from re_tests_plugin import *
+
+def click_comment():
+    lackey.SettingsMaster.MinSimilarity = 0.93
+    lackey.click("tab_comment.png")
+    lackey.SettingsMaster.MinSimilarity = 0.97
+
+def test_1(open_connection):
+    lackey.click("tree_plus.png")
+    all_plus = lackey.findAll("tree_plus.png")
+    lackey.click(list(all_plus)[2])
+    lackey.doubleClick("tree_table_name_EMPLOYEE.png")
+    click_comment()
+    mouse = lackey.Mouse()
+    lackey.click(mouse.getPos().below(100))
+    lackey.type("test comment")
+    lackey.click("icon_commit.png")
+    lackey.click("tab_columns.png")
+    result1 = lackey.exists("table_EMPLOYEE_list_of_columns.png")
+    lackey.click("tab_constraints.png")
+    result2 = lackey.exists("table_EMPLOYEE_list_of_constraints.png")
+    click_comment()
+    lackey.type("a", lackey.Key.CTRL)
+    lackey.type(lackey.Key.BACKSPACE)
+    lackey.click("icon_commit.png")
+    lackey.rightClick("tab_table_EMPLOYEE_blue.png")
+    lackey.click("bt_tab_close_all.png")
+    assert result1 != None
+    assert result2 != None
