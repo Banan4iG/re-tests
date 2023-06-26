d091d3c7ad5b908189c377f8e4e913a6a14af1d8
рома филиппов <roma_filippov_96@bk.ru>
2023-06-26 21:08:55 +0300

    add test_NULL_Trigger

--- tests/test_NULL_Trigger.py
+++ tests/test_NULL_Trigger.py
@@ -2,8 +2,9 @@ import lackey
 from re_tests_plugin import *
 
 def test_1(open_connection):
-    lackey.click("icon_query_editor.png")
+    lackey.click("tab_query_editor_text.png")
     lackey.type("alter trigger fts$trig_22 sql  security definer ")
     lackey.click("icon_execute_query.png")
+    lackey.click("closing_the_query_editor.png")
     result1 = lackey.exists("test_NULL_Trigger.png")
     assert result1 != None
