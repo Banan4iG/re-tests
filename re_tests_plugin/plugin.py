fad428bc22a194cbb27e2d25cd0f6af5e0293811
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-08-07 18:04:34 +0300

    Add atcion after every test esle test result fail
    Because sometimes the child window remains open

--- re_tests_plugin/plugin.py
+++ re_tests_plugin/plugin.py
@@ -9,17 +9,26 @@ def plus_find(name_of_the_group):
 ADMIN_NAME = 'sysdba'
 ADMIN_PASSWORD = 'masterkey'
 
-@pytest.fixture()
-def open_connection():
+@pytest.fixture
+def open_connection(request):
     #actions befor test:
     lackey.App.focus("Red Expert")
     lackey.doubleClick("icon_conn.png")
     time.sleep(2)    #waiting time to open connection
     lackey.click("icon_conn_open.png")
     #actions after test:
+    tests_failed_before = request.session.testsfailed
     yield
+    tests_failed_after = request.session.testsfailed
+    if tests_failed_after > tests_failed_before:
+        bt_cancel = lackey.exists("bt_cancel.png")
+        if bt_cancel != None:
+            lackey.click(bt_cancel)
+            bt_yes = lackey.exists("bt_YES.png")
+            if bt_yes != None:
+                lackey.click(bt_yes)
     lackey.doubleClick("icon_conn_open.png")
-    
+
 @pytest.fixture(scope='session', autouse=True)
 def create_connection():
     lackey.App.focus("Red Expert")
