343c91822ef688a29f395a100695b8158ece168b
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-09-08 15:26:30 +0300

    Add click on propierties tab

--- tests/test_check_fields_after_alter_comment.py
+++ tests/test_check_fields_after_alter_comment.py
@@ -2,9 +2,10 @@ import lackey
 from re_tests_plugin import *
 
 def click_comment():
-    lackey.SettingsMaster.MinSimilarity = 0.93
-    lackey.click("tab_comment.png")
-    lackey.SettingsMaster.MinSimilarity = 0.97
+    lackey.click("tab_properties.png")
+    lackey.click("text_comment.png")
+    mouse = lackey.Mouse()
+    lackey.click(mouse.getPos().below(100))
 
 def test_1(open_connection):
     lackey.click("tree_plus.png")
@@ -12,8 +13,6 @@ def test_1(open_connection):
     lackey.click(plus_find(name_of_the_group))
     lackey.doubleClick("tree_table_name_EMPLOYEE.png")
     click_comment()
-    mouse = lackey.Mouse()
-    lackey.click(mouse.getPos().below(100))
     lackey.type("test comment")
     lackey.click("icon_commit.png")
     lackey.click("tab_columns.png")
