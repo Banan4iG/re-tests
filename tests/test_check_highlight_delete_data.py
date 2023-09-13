ac634ce3a27111a8585a90c03fe10f07ceda5ac7
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-09-13 12:31:35 +0300

    Add test for check correct highlight delete data. See RS-146225

--- tests/test_check_highlight_delete_data.py
+++ tests/test_check_highlight_delete_data.py
@@ -2,7 +2,8 @@ import lackey
 from re_tests_plugin import *
 import time
 
-def test_1(open_connection):
+
+def open_table():
     lackey.click("tree_plus.png")
     name_of_the_group = "icon_tables.png"
     lackey.click(plus_find(name_of_the_group))
@@ -11,9 +12,25 @@ def test_1(open_connection):
     time.sleep(2)
     lackey.click("data_field_1.png")
     lackey.click("icon_delete_record.png")
+
+def test_1(open_connection):
+    open_table()
     result = lackey.exists("data_field_1_red.png")
     lackey.click("tab_columns.png")
     lackey.click("bt_No.png")
     lackey.rightClick("tab_table_EMPLOYEE_blue.png")
     lackey.click("bt_tab_close_all.png")
     assert result != None
+
+
+def test_2(open_connection):
+    open_table()
+    lackey.click("tab_columns.png")
+    lackey.click("bt_No.png")
+    lackey.click("tab_data.png")
+    lackey.SettingsMaster.MinSimilarity = 0.98
+    result = lackey.exists("data_field_1_red.png")
+    lackey.SettingsMaster.MinSimilarity = 0.98
+    lackey.rightClick("tab_table_EMPLOYEE_blue.png")
+    lackey.click("bt_tab_close_all.png")
+    assert result == None
