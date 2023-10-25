14d6e8af62b91f57092add657c3f212261a61a8d
Kashin Nikita <nikita.kashin@red-soft.ru>
2023-10-25 17:48:08 +0300

    Test refactoring

--- tests/test_create_tables_with_space.py
+++ tests/test_create_tables_with_space.py
@@ -5,8 +5,15 @@ def init_create(icon):
     lackey.click("tree_plus.png")
     lackey.rightClick(icon)
     lackey.click("tree_create_menu.png")
-
-def finish_create():
+    lackey.type("a", lackey.Key.CTRL)
+    lackey.type(lackey.Key.BACKSPACE)
+    lackey.type("TEST TABLE")
+    lackey.SettingsMaster.MinSimilarity = 0.96
+    lackey.click(lackey.exists("table_column_Name.png").getTarget().below(20))
+    lackey.SettingsMaster.MinSimilarity = 0.97
+    lackey.type("test")
+    lackey.click(lackey.exists("table_column_Datatype.png").getTarget().below(20))
+    lackey.click("datatype_BIGINT.png")
     lackey.click("bt_OK.png")
     result1 = lackey.exists("text_success.png")
     result2 = lackey.exists("text_test_table.png")
@@ -18,26 +25,6 @@ def finish_create():
 
 def test_create_table(open_connection):
     init_create("icon_tables.png")
-    lackey.type("a", lackey.Key.CTRL)
-    lackey.type(lackey.Key.BACKSPACE)
-    lackey.type("TEST TABLE")
-    lackey.SettingsMaster.MinSimilarity = 0.96
-    lackey.click(lackey.exists("table_colum_Name.png").getTarget().below(20))
-    lackey.SettingsMaster.MinSimilarity = 0.97
-    lackey.type("test")
-    lackey.click(lackey.exists("table_colum_Datatype.png").getTarget().below(20))
-    lackey.click("datatype_BIGINT.png")
-    finish_create()
 
 def test_create_gtt(open_connection):
     init_create("icon_gtt.png")
-    lackey.type("a", lackey.Key.CTRL)
-    lackey.type(lackey.Key.BACKSPACE)
-    lackey.type("TEST TABLE")
-    lackey.SettingsMaster.MinSimilarity = 0.96
-    lackey.click(lackey.exists("table_colum_Name.png").getTarget().below(20))
-    lackey.SettingsMaster.MinSimilarity = 0.97
-    lackey.type("test")
-    lackey.click(lackey.exists("table_colum_Datatype.png").getTarget().below(20))
-    lackey.click("datatype_BIGINT.png")
-    finish_create()
