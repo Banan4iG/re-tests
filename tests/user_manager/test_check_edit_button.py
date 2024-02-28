f5250c4bbde2e81462bf7d6347d9b0561b4e6bf9
Nikita Kashin <nikita.kashin@red-soft.ru>
2024-02-28 11:48:35 +0300

    Fix tests and image

--- tests/user_manager/test_check_edit_button.py
+++ tests/user_manager/test_check_edit_button.py
@@ -11,9 +11,14 @@ def test_check_edit_button(open_connection):
     lackey.click("tab_user_manager.png")
     lackey.click("text_TEST_USER.png")
     lackey.click("tree_edit_menu.png")
-    lackey.click(lackey.exists("text_first_name.png").getTarget().right(200))
-    lackey.type("first{TAB}middle{TAB}last{TAB}{SPACE}{TAB}{SPACE}{TAB}description")
+    lackey.click(lackey.exists("text_first_name.png").getTarget().right(100))
+    lackey.type("first{TAB}middle{TAB}last{TAB}{SPACE}{TAB}{SPACE}")
+    lackey.click("tab_comment.png")
+    mouse = lackey.Mouse()
+    lackey.click(mouse.getPos().offset(30, 100))
+    lackey.type("description")
     lackey.click("bt_OK.png")
+    lackey.click("bt_commit.png")
     with fdb.connect("employee") as con:
         cur = con.cursor()
         cur.execute("""select cast(sec$user_name as VARCHAR(9)), 
