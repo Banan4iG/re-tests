8b39d13d9c744302544b34104eed6b003edfa128
ThatKirbyGuy <114916098+ThatKirbyGuy@users.noreply.github.com>
2024-03-18 23:43:06 +0300

    Add files via upload

--- tests/profiler/test_check_profiler_checkboxes.py
+++ tests/profiler/test_check_profiler_checkboxes.py
@@ -18,13 +18,14 @@ def test_check_profiler_checkboxes(open_connection):
     lackey.click("bt_commit.png")
     lackey.click("pause.png")
     lackey.click("text_Default_view.png")
-    result1 = lackey.exists("text_Default_checkbox.png")
+    result1 = lackey.exists("icon_profiler_default.png")
     lackey.click("text_Compact_view.png")
+    result2 = lackey.exists("icon_profiler_compact.png")
     lackey.click("text_Extended_view.png")
-    result2 = lackey.exists("text_Extended_checkbox.png")
+    result3 = lackey.exists("checkbox_Extended.png")
     lackey.click("text_Compact_view.png")
     lackey.click("text_Round_Values.png")
-    result3 = lackey.exists("text_Round_checkbox.png")
+    result4 = lackey.exists("text_Round_checkbox.png")
     lackey.click("text_Round_Values.png")
     lackey.click("discard.png")
     lackey.click("icon_cross.png")
@@ -34,3 +35,4 @@ def test_check_profiler_checkboxes(open_connection):
     assert result1 != None
     assert result2 != None
     assert result3 != None
+    assert result4 != None
