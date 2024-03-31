4905ec3079d08a5d0ea2e6226ed4b4a852e29554
ThatKirbyGuy <114916098+ThatKirbyGuy@users.noreply.github.com>
2024-03-31 23:19:30 +0300

    Add files via upload

--- tests/profiler/test_check_profiler_execute.py
+++ tests/profiler/test_check_profiler_execute.py
@@ -1,20 +1,13 @@
 import lackey
 from re_tests_plugin import *
-import firebird.driver as fdb
 
 def test_check_profiler_execute(open_connection):
     lackey.click("tab_query_editor_text.png")
-    lackey.type("CREATE TABLE TEST (")
-    lackey.type("{ENTER}")
-    lackey.type("{TAB}")
-    lackey.type("TEST BIGINT)")
+    lackey.type("SELECT * FROM JOB")
     lackey.click("icon_profiler_execute.png")
     result1 = lackey.exists("icon_profiler_select.png")
     lackey.click("discard.png")
     lackey.click("icon_cross.png")
     lackey.type("a", lackey.Key.CTRL)
     lackey.type("{BACKSPACE}")
-    with fdb.connect("employee") as con:
-        con.execute_immediate("DROP TABLE TEST;")
-        con.commit()
     assert result1 != None
