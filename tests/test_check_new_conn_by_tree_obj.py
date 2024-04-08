9e72872ba62e49ee982e98139247216610d69e42
Nikita Kashin <nikita.kashin@red-soft.ru>
2024-04-08 15:09:32 +0300

    Add tests for database connectivity immediately after the newly populated data. See RS-98655

---
+++ tests/test_check_new_conn_by_tree_obj.py
@@ -0,0 +1,41 @@
+import lackey
+import keyboard
+from re_tests_plugin import *
+
+def create_new_conn():
+    keyboard.press('ctrl')
+    lackey.type('n', lackey.Key.SHIFT)
+    keyboard.release('ctrl')
+    lackey.type("{TAB}"*6)
+    lackey.type("employee.fdb")
+    lackey.type("{TAB}"*8)
+    lackey.type("sysdba{TAB}masterkey")
+
+def remove_conn():
+    lackey.doubleClick("icon_conn_open.png")
+    list_b = list(lackey.findAll("icon_conn.png"))
+    b = max(list_b, key=lambda i: i.getTarget().getY())
+    lackey.rightClick(b.getTarget())
+    lackey.click("tree_remove_conn.png")
+    lackey.type("{ENTER}")
+    lackey.rightClick("icon_abstract_object.png")
+    lackey.click("bt_tab_close_all.png")
+
+def test_1():
+    create_new_conn()
+    list_b = list(lackey.findAll("icon_conn.png"))
+    b = max(list_b, key=lambda i: i.getTarget().getY())
+    lackey.rightClick(b.getTarget())	
+    lackey.click("tree_connect.png")
+    result = lackey.exists("icon_conn_open.png") 
+    remove_conn()
+    assert result != None
+
+def test_2():
+    create_new_conn()
+    list_b = list(lackey.findAll("icon_conn.png"))
+    b = max(list_b, key=lambda i: i.getTarget().getY())
+    lackey.doubleClick(b.getTarget())
+    result = lackey.exists("icon_conn_open.png") 
+    remove_conn()
+    assert result != None
