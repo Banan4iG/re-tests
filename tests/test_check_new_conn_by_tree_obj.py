8cccff842de45cc067388e01821f85dc455236b8
Nikita Kashin <nikita.kashin@red-soft.ru>
2024-04-11 15:14:24 +0300

    Add delay to wait for creating new Database Browser tab

--- tests/test_check_new_conn_by_tree_obj.py
+++ tests/test_check_new_conn_by_tree_obj.py
@@ -6,6 +6,7 @@ def create_new_conn():
     keyboard.press('ctrl')
     lackey.type('n', lackey.Key.SHIFT)
     keyboard.release('ctrl')
+    time.slep(2)
     lackey.type("{TAB}"*6)
     lackey.type("employee.fdb")
     lackey.type("{TAB}"*8)
