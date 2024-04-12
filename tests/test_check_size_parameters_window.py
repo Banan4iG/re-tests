8c653953f93b638d79ed74a3ffde10feb55b1775
Nikita Kashin <nikita.kashin@red-soft.ru>
2024-04-12 14:59:28 +0300

    Small refactoring test

--- tests/test_check_size_parameters_window.py
+++ tests/test_check_size_parameters_window.py
@@ -8,14 +8,16 @@ def test_1(open_connection):
     lackey.click("tab_query_editor_text.png")
     lackey.type("select cast(:test as integer) from rdb$database")
     lackey.click("icon_execute_query.png")
-    result1 = lackey.exists("ms_text_input_parameters_start.png")
-    result2 = lackey.exists("ms_text_input_parameters_end.png")
+    start = lackey.exists("ms_text_input_parameters_start.png")
+    x_start = start.getTopLeft().getX()
+    end = lackey.exists("ms_text_input_parameters_end.png")
+    x_end = end.getTopRight().getX()
+    print(x_end-x_start)
     lackey.type("123456")
-    result3 = lackey.exists("ms_input_parameters_with_int.png")
+    result = lackey.exists("ms_input_parameters_with_int.png")
     lackey.click("bt_cancel.png")
     time.sleep(1)
     lackey.type("a", lackey.Key.CTRL)
     lackey.type(lackey.Key.BACKSPACE)
-    assert result1 != None
-    assert result2 != None
-    assert result3 != None
+    assert x_end-x_start >= 200
+    assert result != None
